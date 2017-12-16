31a5fa1eba43ccaff0a06bc08c0a6f351f3cee41
James Grams <james@game103.net>
2017-12-16 18:45:56 -0500

    Fixed cache

--- scripts/cacher.py
+++ scripts/cacher.py
@@ -16,7 +16,10 @@ def crawl(root):
 	try:
 		print "Crawling " + root + "..."
 		# create the request
-		request = urllib2.Request(base_url + root, headers={"Accept" : "application/xml"})
+		add_on = "?no_cache=1"
+		if "?" in root:
+			add_on = "&no_cache=1"
+		request = urllib2.Request(base_url + root + add_on, headers={"Accept" : "application/xml"})
 		context = ssl.SSLContext(ssl.PROTOCOL_TLSv1)
 		# read
 		u = urllib2.urlopen(request, context=context)
@@ -29,15 +32,15 @@ def crawl(root):
 			# we don't want to crawl links we've already been to
 			if not link in pages and not link in bad_pages:
 				# add ws if we want to 
-				crawl(link + "?no_cache=1")
+				crawl(link)
 				if re.match('^\/games[^?]*$|^\/videos[^?]*$^\/everything[^?]*$|^\/apps[^?]*$|^\/resources[^?]*$', link):
-					crawl(link + '?ws=1&no_cache=1')
+					crawl(link + '?ws=1')
 	except Exception as e:
 		print e
 		# Occurs if there is a 500 error
 		bad_pages[root] = root
 
-crawl('/?no_cache=1');
+crawl('/');
 
 os.chdir('/var/www/game103/cache')
 
