660dbfdde4a642ab90568505437f69d408a73424
James Grams <james@game103.net>
2017-12-19 21:15:27 -0500

    Updated FB meta

--- index.php
+++ index.php
@@ -80,6 +80,7 @@
 			$title = $generated['name'];
 			$description = $generated['description'] . "Play $title on Game 103!";
 			$meta = "<meta property='og:image' content='http://game103.net{$generated['image_url']}'>
+			<meta property='og:title' content='$title'>
 			<meta property='og:description' content=\"{$generated['description']}\">";
 			break;
 		case 'games':
