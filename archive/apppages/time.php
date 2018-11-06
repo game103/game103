68d657b38475e484bd86df7642901c8044fce8f8
root <root@game103.net>
2018-11-06 09:09:33 -0500

    Cleanup

--- archive/apppages/time.php
+++
@@ -1,8 +0,0 @@
-<?php
-date_default_timezone_set('America/New_York');
-$date = date('m-d-Y');
-$time = date('h:i:s');
-echo "<div style='text-align:center;'>$time";
-echo "<img src = '../images/cocoarunning.gif' height = 225 width = 350>";
-echo "$date</div>";
-?>
