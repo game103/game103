bf26aaa70a8b009b92937daf9a2e32b74cddc822
James Grams <james@game103.net>
2018-11-07 13:18:34 -0500

    Added hook

---
+++ ws/hook.php
@@ -0,0 +1,29 @@
+<?php
+
+	error_reporting(E_ERROR);
+
+	/**
+	 * Script to receieve a web hook from GitHub
+	 */
+
+	set_include_path( $_SERVER['DOCUMENT_ROOT'] . "/" . "modules" );
+
+	// Require modules
+	require_once( 'Constants.class.php');
+
+	// Receive POST data
+	$post_data = file_get_contents('php://input');
+
+	// HMAC the data using the secret token
+	// We'll make sure this matches the data GitHub sent us
+	$signature = hash_hmac('sha1', $post_data, Constants::GITHUB_WEBHOOK_TOKEN);
+
+	// If the signature is correct
+	if( $_SERVER['HTTP_X_HUB_SIGNATURE'] == 'sha1=' . $signature ) {
+		exec("touch /var/www/game103/ws/test.txt");
+	}
+	else {
+		http_response_code( 403 );
+		die("Forbidden\n");
+	}
+?>
