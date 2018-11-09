bd1e7ea7e1cb7647cd83aca26d07c83a90e8ba5e
James Grams <james@game103.net>
2018-11-09 08:13:07 -0500

    Fixed homepage css

--- modules/Widget/Home.class.php
+++ modules/Widget/Home.class.php
@@ -61,6 +61,7 @@
 										),
 					'title'			=> "Top Games",
 					'footer'		=> "",
+					'id'			=> "top-games"
 				) );
 				$top_box->generate();
 				
@@ -71,6 +72,7 @@
 										),
 					'title'			=> "Featured Games",
 					'footer'		=> "",
+					'id'			=> "featured-games"
 				) );
 				$featured_box->generate();
 				
