9885eb549a98c2e310c96cceb30a1f7548846b8c
James Grams <james@game103.net>
2018-11-09 08:49:39 -0500

    Updated reboot

--- scripts/minor_reboot.sh
+++ scripts/minor_reboot.sh
@@ -1,11 +1,12 @@
 /etc/init.d/cron stop
 cd /var/www/game103
+git reset --hard
 git pull
 git submodule foreach git pull origin master
 find . -name "*.fla" -type f | xargs rm -f
 find . -name "*.as" -type f|xargs rm -f
 find . -name "*.ipa" -type f|xargs rm -f
 find . -name "*.apk" -type f|xargs rm -f
-find /var/www/game103/css ! -name "*.min.css" -not -path '/var/www/game103/css' -exec sh -c 'uglifycss {} > $(echo {} | cut -f 1 -d "." | xargs -L1 -I '"'"'$'"'"' echo '"'"'$.css'"'"')' \;
-find /var/www/game103/javascript ! -name "*.min.js" -not -path '/var/www/game103/javascript' -exec sh -c 'uglifyjs {} > $(echo {} | cut -f 1 -d "." | xargs -L1 -I '"'"'$'"'"' echo '"'"'$.js'"'"')' \;
+find /var/www/game103/css -maxdepth 1 -iname "*.css" -exec uglifycss --compress --mangle -o {} -- {} \;
+find /var/www/game103/javascript -maxdepth 1 -iname "*.js" -exec uglifyjs --compress --mangle -o {} -- {} \;
 /etc/init.d/cron start
