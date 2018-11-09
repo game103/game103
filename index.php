26934f1fdd205a73b431261b8d3b5e7d5e95f225
James Grams <james@game103.net>
2018-11-09 17:51:45 -0500

    Add GitHub as a source

--- index.php
+++ index.php
@@ -645,7 +645,7 @@
 			</div>
 			<!--Contact and Copyright-->
 			<div class="footer">
-				<div class="additional-links"><a href="/about">About Us</a> | <a href="/privacy">Privacy Policy</a> | <a href="/facts">Fun Facts</a> | <a href="/characters">Characters</a> | <a href="/games/distributable">Developers</a></div>
+				<div class="additional-links"><a href="/about">About Us</a> | <a href="/privacy">Privacy Policy</a> | <a href="/facts">Fun Facts</a> | <a href="/characters">Characters</a> | <a href="/games/distributable">Developers</a> | <a href="https://github.com/game103/game103">Source</a></div>
 				<div class="copyright">&copy; 2018 <a href="https://game103.net">Game 103</a></div>
 			</div>
 		</div>
