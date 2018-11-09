05b9fb9bad8999499cdd028bc2dfd1fbcc4200e8
James Grams <james@game103.net>
2018-11-08 19:53:32 -0500

    Updated hooks

--- scripts/full_reboot.sh
+++ scripts/full_reboot.sh
@@ -1,4 +1,4 @@
-etc/init.d/crond/stop
+etc/init.d/cron stop
 cd /var/www/
 git clone https://github.com/game103/game103.git game103_new
 cd game103_new
@@ -10,8 +10,8 @@ find . -name "*.apk" -type f|xargs rm -f
 cd ..
 cp -R game103_private/* game103_new
 find /var/www/game103_new/css ! -name "*.min.css" -not -path '/var/www/game103_new/css' -exec sh -c 'uglifycss {} > $(echo {} | cut -f 1 -d "." | xargs -L1 -I '"'"'$'"'"' echo '"'"'$.min.css'"'"')' \;
-find /var/www/game103_new/js ! -name "*.min.js" -not -path '/var/www/game103_new/js' -exec sh -c 'uglifyjs {} > $(echo {} | cut -f 1 -d "." | xargs -L1 -I '"'"'$'"'"' echo '"'"'$.min.css'"'"')' \;
+find /var/www/game103_new/javascript ! -name "*.min.js" -not -path '/var/www/game103_new/javascript' -exec sh -c 'uglifyjs {} > $(echo {} | cut -f 1 -d "." | xargs -L1 -I '"'"'$'"'"' echo '"'"'$.min.js'"'"')' \;
 mv game103_new game103
 python /var/www/game103/scripts/cacher.py
 /var/www/game103/scripts/border_maker.sh
-/etc/init.d/crond/start
+/etc/init.d/cron start
