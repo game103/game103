aa676a7a7be47910aadf7e18bc68f476da8da52c
James Grams <james@game103.net>
2018-11-23 22:33:29 -0500

    Manifest location and js narrow fix

--- index.php
+++ index.php
@@ -605,7 +605,7 @@
 		<meta http-equiv="Content-Type" content="text/html;charset=utf-8">
 		<meta name="viewport" content="width=device-width,initial-scale=1,user-scalable=no">
 		<link rel="apple-touch-icon" href="/images/logoiconsocialmedia.png">
-		<link rel="manifest" href="manifest.json" />
+		<link rel="manifest" href="/manifest.json" />
 		<?php echo $meta ?>
 		
 		<!-- Title -->
