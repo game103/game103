a24275ee618cc9dc0aa7f747fab702dbb2566563
James Grams <james@game103.net>
2018-11-24 14:47:43 -0500

    Fixed nav

--- scripts/db_backup.sh
+++ scripts/db_backup.sh
@@ -1,3 +1,3 @@
-mysqldump -u root -p***REMOVED*** --all-databases > /home/james/db_backup.sql
+mysqldump -u root -pYellowTang704@ndPups --all-databases > /home/james/db_backup.sql
 DATE=`date +%Y-%m-%d`
-sshpass -p "***REMOVED***" scp /home/james/db_backup.sql hallaby@129.121.31.190:~/db_backups/db_backup_$DATE.sql
+sshpass -p "YellowTang704@ndPups" scp /home/james/db_backup.sql hallaby@129.121.31.190:~/db_backups/db_backup_$DATE.sql
