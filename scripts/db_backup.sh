d4b9256e2b930fc197fc8b9bb471af73ce7a2652
James Grams <james@game103.net>
2019-02-20 13:51:27 -0500

    Updated db backup

--- scripts/db_backup.sh
+++ scripts/db_backup.sh
@@ -1,4 +1,4 @@
 P=`php -r "include '/var/www/game103/modules/Constants.class.php'; echo Constants::DB_PASSWORD;"`
 mysqldump -u root -p$P --all-databases > /home/james/db_backup.sql
 DATE=`date +%Y-%m-%d`
-scp /home/james/db_backup.sql james@10.0.0.170:/mnt/c/Users/James/Documents/Archives/Database
+scp /home/james/db_backup.sql james@10.0.0.65:/home/james/backup/mysql/
