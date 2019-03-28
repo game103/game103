a5c23f515b12edc98316208f7c548cd08b4d01c3
James Grams <james.grams@gordon.edu>
2019-03-28 09:42:58 -0400

    Added levenshtein search to everything search

--- modules/Service/Find.class.php
+++ modules/Service/Find.class.php
@@ -41,6 +41,9 @@
 		// The key is the display and the value
 		// is the link
 		protected $linked_pages;
+
+		// Whether levenshtein search is enabled
+		protected $levenshtein_search_enabled; 
 		
 		/**
 		* Constructor.
@@ -71,6 +74,11 @@
 			);
 			$this->valid_platforms = array();
 			$this->linked_pages = array();
+
+			$this->levenshtein_search_enabled = false;
+			if( \Constants::LEVENSHTEIN_SEARCH_ENABLED && strlen($this->search) >= 3 ) {
+				$this->levenshtein_search_enabled = true;
+			}
 			// It is important that we don't error check yet unless the above arrays are altered
 		}
 		
@@ -119,7 +127,7 @@
 		* Generate where sql for a specified table.
 		*/
 		protected function generate_where( $table ) {
-			if($this->search != '') {
+			if($this->search != '' && !$this->levenshtein_search_enabled) {
 				return "WHERE $table.name LIKE ?";
 			}
 			else {
@@ -144,6 +152,10 @@
 			$apps_sql = $this->generate_where('apps');
 			$downloads_sql = $this->generate_where('downloads');
 			$sort_sql = $this->generate_sort();
+			$limit = "LIMIT $items_per_page OFFSET $offset";
+			if( $this->levenshtein_search_enabled ) {
+				$limit = "";
+			}
 			$select_str = "
 					SELECT * FROM(
 					SELECT name, description, url_name, image_url, rating, FORMAT(plays, 0), plays as numeric_interactions, added_date, -1 as store_url_android, -1 as store_url_apple, -1 as type, 'game', type as game_type FROM hallaby_games.entries $where_sql
@@ -154,8 +166,8 @@
 					UNION
 					SELECT name, description, url_name, image_url, -1 as rating, FORMAT(visits, 0), visits as numeric_interactions, added_date, store_url_android, store_url_apple, type, 'app', -1 as game_type FROM hallaby_games.apps $apps_sql
 					ORDER BY $sort_sql
-					LIMIT $items_per_page
-					OFFSET $offset) AS main
+					$limit
+					) AS main
 					LEFT JOIN (
 					SELECT sum(c) AS total_count
 					FROM
@@ -214,9 +226,10 @@
 		*/
 		protected function parse_result( $select_statement ) {
 			$select_statement->bind_result( $name, $description, $url_name, $image_url, $rating, $interactions, $numeric_interactions, $added_date, $store_url_android, $store_url_apple, $app_type, $item_type, $game_type, $total_count );
-			
+
 			$items = array();
 			while($select_statement->fetch()) {
+
 				$item_object = array (
 					"title" => htmlentities($name, ENT_QUOTES),
 					"description" => $description,
@@ -231,10 +244,165 @@
 					"added_date" => $added_date,
 					"game_type" => $game_type,
 				);
+
 				$items[] = $item_object;
 			}
 			$select_statement->close();
+
 			return $this->supplement_items( $items, $total_count );
+		}
+
+		/**
+		 * Parse the result of a sql statement using similar text
+		 */
+		protected function parse_result_similar_text( $select_statement ) {
+			$select_statement->bind_result( $name, $description, $url_name, $image_url, $rating, $interactions, $numeric_interactions, $added_date, $store_url_android, $store_url_apple, $app_type, $item_type, $game_type, $total_count );
+		
+			$manual_count = 0;
+			$items = array();
+			while($select_statement->fetch()) {
+				
+				$ratio = $this->levenshtein_ratio( $this->search, $name, true );
+
+				// If we have similar text match enabled, we do the search filter in php
+				if( $ratio['ratio'] > 85 || $ratio['boosted'] > 92 || $ratio['max_part_ratio'] > 70 || $ratio['word_ratio_boosted'] > 80 ) {
+
+					$item_object = array (
+						"title" => htmlentities($name, ENT_QUOTES),
+						"description" => $description,
+						"image_src" => $image_url,
+						"count" => $interactions,
+						"url_name"	=> $url_name,
+						"rating" => $rating,
+						"type" => $item_type,
+						"app_type" => $app_type,
+						"store_url_android" => $store_url_android,
+						"store_url_apple" => $store_url_apple,
+						"added_date" => $added_date,
+						"game_type" => $game_type,
+						"ratio" => $ratio['ratio'],
+						"max_part_ratio" => $ratio['max_part_ratio'],
+						"boosted" => $ratio['boosted'],
+						"word_ratio_boosted" => $ratio['word_ratio_boosted'],
+						"name" => $name,
+						"original_place" => $manual_count
+					);
+	
+					$items[] = $item_object;
+					$manual_count ++;
+
+				}
+
+			}
+
+			// Sort by match num (how well matched), then string length diff absolute value, then string length (more text = more like what user wanted), then actual sort (popularity, etc.)
+			usort($items, function ($a, $b) { 
+				if( $a['word_ratio_boosted'] > $b['word_ratio_boosted'] ) { return -1; }
+				if( $a['word_ratio_boosted'] < $b['word_ratio_boosted'] ) { return 1; }
+				if( $a['max_part_ratio'] > $b['max_part_ratio'] ) { return -1; }
+				if( $a['max_part_ratio'] < $b['max_part_ratio'] ) { return 1; }
+				/*if( $a['ratio'] > $b['ratio'] ) { return -1; }
+				if( $a['ratio'] < $b['ratio'] ) { return 1; }*/
+				/*if( $a['boosted'] > $b['boosted'] ) { return -1; }
+				if( $a['boosted'] < $b['boosted'] ) { return 1; }*/
+				if( $a['original_place'] < $b['original_place'] ) { return -1; }
+				if( $a['original_place'] > $b['original_place'] ) { return 1; }
+				return 0;
+			});
+
+			$select_statement->close();
+
+			$items_per_page = $this->items_per_page;
+			$offset = $this->generate_offset();
+			$paginated_items = array_splice( $items, $offset, $items_per_page );
+
+			return $this->supplement_items( $paginated_items, $manual_count );
+		}
+
+		/**
+		 * Find the levenshtein ratio between a search and a name.
+		 * Future suggestions
+		 * sooper bouncy should have same results as bouncy sooper, do you need to get permutations after all for part ratios?
+		 * jellybears > should find jelly bears. we should create all possible combined words, but full ratio matches for now.
+		 * stemming
+		 * prefix favorability
+		 */
+		protected function levenshtein_ratio( $search, $name, $substrings ) {
+			
+			if( $substrings ) { // should be already fixed with substrings
+				// Fix the phrases
+				$search = strtolower( $search );
+				$name = strtolower( $name );
+				$search = str_replace( "é", "e", $search );
+				$name = str_replace( "é", "e", $name );
+				$search = preg_replace( "/[\?<>,\.;:'\[\]{}\(\)\*&^%$#@!]/", "", $search );
+				$name = preg_replace( "/[\?<>,\.;:'\[\]{}\(\)\*&^%$#@!]/", "", $name );
+				// Remove stopwords
+				$stopwords = array("a", "about", "above", "after", "again", "against", "ain", "all", "am", "an", "and", "any", "are", "aren", "aren't", "as", "at", "be", "because", "been", "before", "being", "below", "between", "both", "but", "by", "can", "couldn", "couldn't", "d", "did", "didn", "didn't", "do", "does", "doesn", "doesn't", "doing", "don", "don't", "down", "during", "each", "few", "for", "from", "further", "had", "hadn", "hadn't", "has", "hasn", "hasn't", "have", "haven", "haven't", "having", "he", "her", "here", "hers", "herself", "him", "himself", "his", "how", "i", "if", "in", "into", "is", "isn", "isn't", "it", "it's", "its", "itself", "just", "ll", "m", "ma", "me", "mightn", "mightn't", "more", "most", "mustn", "mustn't", "my", "myself", "needn", "needn't", "no", "nor", "not", "now", "o", "of", "off", "on", "once", "only", "or", "other", "our", "ours", "ourselves", "out", "over", "own", "re", "s", "same", "shan", "shan't", "she", "she's", "should", "should've", "shouldn", "shouldn't", "so", "some", "such", "t", "than", "that", "that'll", "the", "their", "theirs", "them", "themselves", "then", "there", "these", "they", "this", "those", "through", "to", "too", "under", "until", "up", "ve", "very", "was", "wasn", "wasn't", "we", "were", "weren", "weren't", "what", "when", "where", "which", "while", "who", "whom", "why", "will", "with", "won", "won't", "wouldn", "wouldn't", "y", "you", "you'd", "you'll", "you're", "you've", "your", "yours", "yourself", "yourselves", "could", "he'd", "he'll", "he's", "here's", "how's", "i'd", "i'll", "i'm", "i've", "let's", "ought", "she'd", "she'll", "that's", "there's", "they'd", "they'll", "they're", "they've", "we'd", "we'll", "we're", "we've", "what's", "when's", "where's", "who's", "why's", "would");
+				foreach( $stopwords as $stopword ) {
+					$search = preg_replace( '/^'.$stopword.'[\s-_]|[\s-_]'.$stopword.'[\s-_]|[\s-_]'.$stopword.'$/', " ", $search );
+					$name = preg_replace( '/^'.$stopword.'[\s-_]|[\s-_]'.$stopword.'[\s-_]|[\s-_]'.$stopword.'$/', " ", $name );
+				}
+				$search = trim($search);
+				$name = trim($name);
+			}
+
+			$search_length = strlen($search);
+			$name_length = strlen($name);
+			$greater = $name_length;   
+  			if( $search_length > $name_length ) { $greater = $search_length; }
+			$match_percent = (1 - levenshtein($search, $name)/$greater) * 100;
+			// Boosted will match any successive characters in the string (so search is "boo", "Big Old Octopus" would be 100 - this is why we have phrase and word match)
+			// Boost is pretty sloppy
+			$boost = min( max( 1, $name_length/$search_length ), 4 );
+
+			// If substrings is true, we generate in order substrings of the name
+			// of length - the same number of words as the search. We then find the maximum
+			// levenshtein ratio of all the substrings compared to the search and include that in the result.
+			// This allows us to favor phrases with matching terms that directly follow one another
+			// (e.g. for "bald eagle" favor "bald eagle game" over "help the bald man find the eagle")
+			if( $substrings ) {
+
+				$search_parts = preg_split( "/[\s-_]/", $search );
+				$search_num_of_words = count($search_parts);
+				$name_parts = preg_split( "/[\s-_]/", $name );
+				$max_part_ratio = 0;
+				for( $i=0; $i<count($name_parts); $i++ ) {
+
+					// We can have equal since the index used will be one less
+					if( $i + $search_num_of_words <= count($name_parts) ) {
+
+						$part_ratio = $this->levenshtein_ratio( $search, implode( array_slice( $name_parts, $i, $search_num_of_words ) ), false );
+						if( $part_ratio['ratio'] > $max_part_ratio ) {
+							$max_part_ratio = $part_ratio['ratio'];
+						}
+
+					}
+
+				}
+				
+				// For each of the search parts, get the maximum of all the
+				// levenshtein distances between this search part and each of the name parts
+				// Then, return to minimum of all the previously calulated values for the search parts
+				// This allows us to match terms that just have all the right words (even if in wrong order)
+				$min_word_dist = 100;
+				for( $i=0; $i<count($search_parts); $i++) {
+					$max_word_dist = 0;
+					for( $j=0; $j<count($name_parts); $j++ ) {
+						$dist = $this->levenshtein_ratio( $search_parts[$i], $name_parts[$j], false );
+						// We use boosted to allow for substrings
+						if( $dist['boosted'] > $max_word_dist ) {
+							$max_word_dist = $dist['boosted'];
+						}
+					}
+					if( $max_word_dist < $min_word_dist ) {
+						$min_word_dist = $max_word_dist;
+					}
+				}
+
+			}
+
+			return array( 'ratio' => round($match_percent, 2), "boosted" => round($match_percent * $boost, 2), "max_part_ratio" => round($max_part_ratio, 2), "word_ratio_boosted" => round($min_word_dist, 2) );
 		}
 		
 		/**
@@ -298,6 +466,9 @@
 			
 			try {
 				$this->error_check();
+				if( $this->levenshtein_search_enabled ) {
+					return $this->parse_result_similar_text( $this->run_sql ( $this->bind_params() ) );
+				}
 				return $this->parse_result( $this->run_sql ( $this->bind_params() ) );
 			}
 			catch (\Exception $e) {
