0e414bc5842e21d56b84aa31e26a1d75745c1a26
James Grams <james@game103.net>
2019-04-09 10:21:27 -0400

    Updated FB Graph API version

--- scripts/dailygame.php
+++ scripts/dailygame.php
@@ -75,6 +75,7 @@
 	$config['app_id'] = Constants::FB_APP_ID;
 	$config['app_secret'] = Constants::FB_APP_SECRET;
 	$config['fileUpload'] = false; // optional
+	$config['default_graph_version'] = "v3.2";
 	 
 	$fb = new Facebook\Facebook($config);
 	 
