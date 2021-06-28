817c23637b24176bcdb4723c887927de4ba0e78e
James Grams <james@game103.net>
2021-06-28 10:07:30 -0400

    Fix contact page

--- ws/contact.php
+++ ws/contact.php
@@ -1,12 +1,12 @@
 <?php
-	error_reporting(0);
+    error_reporting(0);
     
-	set_include_path($_SERVER['DOCUMENT_ROOT']  . "/" . "modules");
+    set_include_path($_SERVER['DOCUMENT_ROOT']  . "/" . "modules");
 
-	// Require modules
-	require_once( 'Constants.class.php');
+    // Require modules
+    require_once( 'Constants.class.php');
 	
-	$email = strip_tags($_POST['email']);
+    $email = strip_tags($_POST['email']);
     $valid_domains = array(
         "game103.net",
         "evenlode.dev",
@@ -16,9 +16,10 @@
         "jamesgrams.com"
     );
     $http_origin = $_SERVER['HTTP_ORIGIN'];
+    $http_domain = parse_url($http_origin)['host'];
     $domain = array_pop(explode('@', $email));
     $status = "failure";
-    if( $domain == $http_origin && in_array($domain, $valid_domains) ) {
+    if( $domain == $http_domain && in_array($domain, $valid_domains) ) {
         header("Access-Control-Allow-Origin: $http_origin");
         $to_email = $email;
         $subject = $_POST['subject'];
@@ -27,8 +28,8 @@
         $headers .= "MIME-Version: 1.0\r\n";
         $headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
         $message = $_POST['body'];
-        mail($to_email,$subject,$message,$headers);
-        $status = "success";
+        $result = mail($to_email,$subject,$message,$headers);
+        if( $result ) $status = "success";
     }
     
     // We will always show success, even if no ID
