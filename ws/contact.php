fba25ee0ac1ca5091e69ba7bc8e726aac011cebd
James Grams <james@game103.net>
2021-06-28 09:26:48 -0400

    Add contact web service

---
+++ ws/contact.php
@@ -0,0 +1,39 @@
+<?php
+	error_reporting(0);
+    
+	set_include_path($_SERVER['DOCUMENT_ROOT']  . "/" . "modules");
+
+	// Require modules
+	require_once( 'Constants.class.php');
+	
+	$email = strip_tags($_POST['email']);
+    $valid_domains = array(
+        "game103.net",
+        "evenlode.dev",
+        "makingdorecipes.com",
+        "smartsteplavs.com",
+        "guystation.net",
+        "jamesgrams.com"
+    );
+    $http_origin = $_SERVER['HTTP_ORIGIN'];
+    $domain = array_pop(explode('@', $email));
+    $status = "failure";
+    if( $domain == $http_origin && in_array($domain, $valid_domains) ) {
+        header("Access-Control-Allow-Origin: $http_origin");
+        $to_email = $email;
+        $subject = $_POST['subject'];
+        $headers = "From: james@game103.net\r\n";
+        $headers .= "Reply-To: james@game103.net\r\n";
+        $headers .= "MIME-Version: 1.0\r\n";
+        $headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
+        $message = $_POST['body'];
+        mail($to_email,$subject,$message,$headers);
+        $status = "success";
+    }
+    
+    // We will always show success, even if no ID
+    // so people can't check if emails were successful or not
+    echo json_encode ( array(
+        'status'   => $status
+    ) );
+?>
